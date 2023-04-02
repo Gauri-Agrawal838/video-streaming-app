047fc5e7c30c15587a0fedc10369c846a65f561a
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-02 21:39:39 +0530

    initial commit

---
+++ src/redux/actions/auth.action.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import auth from '../../firebase'
+
+const login = () => async dispatch => {
+    try {
+
+        const provider = new firebase.auth.GoogleAuthProvider()
+
+    } catch (error) {
+
+    }
+}
