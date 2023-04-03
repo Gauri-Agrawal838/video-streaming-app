2f489d238cd53ae6ba4032880fbcd72ce17fa09a
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-03 19:40:10 +0530

    firebase auth added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAismaZYPY62RI96aguRYQ0MWwyX9KPXVc",
+    authDomain: "video-streaming-app2.firebaseapp.com",
+    projectId: "video-streaming-app2",
+    storageBucket: "video-streaming-app2.appspot.com",
+    messagingSenderId: "299713106635",
+    appId: "1:299713106635:web:d5d3fb6d302c4fd8c4c14e"
+  };
+
+firebase.initializeApp(firebaseConfig)
