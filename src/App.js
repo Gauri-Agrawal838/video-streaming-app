0bb03507c5297dfda24248bedc83c3cc22990ae9
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-03 19:40:02 +0530

    firebase auth added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
 
-import HomeScreen from "./screens/homeScreen/HomeScreen";
-import LoginScreen from './screens/loginScreen/LoginScreen';
-import Header from './components/header/Header';
-import SideBar from './components/sidebar/SideBar';
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar'
+import HomeScreen from './screens/homeScreen/HomeScreen'
+import LoginScreen from './screens/loginScreen/LoginScreen'
 
-import "./_app.scss";
-import { useState } from "react";
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+
+import './_app.scss'
+import { useSelector } from 'react-redux'
+
+const Layout = ({ children }) => {
+   const [sidebar, toggleSidebar] = useState(false)
+
+   const handleToggleSidebar = () => toggleSidebar(value => !value)
+
+   return (
+      <>
+         <Header handleToggleSidebar={handleToggleSidebar} />
+         <div className='app__container'>
+            <Sidebar
+               sidebar={sidebar}
+               handleToggleSidebar={handleToggleSidebar}
+            />
+            <Container fluid className='app__main '>
+               {children}
+            </Container>
+         </div>
+      </>
+   )
+}
 
 const App = () => {
+   return (
+    <Router>
+      <Switch>
+         <Route path='/' exact>
+            <Layout>
+               <HomeScreen />
+            </Layout>
+         </Route>
 
-    const [sideBar, toggleSidebar] = useState(false);
-    const handleToggleSidebar = () => toggleSidebar(value => !value)
-    return (
-        <>
-            <Header handleToggleSidebar={handleToggleSidebar} />
-            <div className="app__container border border-info">
-                <SideBar sideBar={sideBar}
-                    handleToggleSidebar={handleToggleSidebar}
-                />
-                <Container fluid className="app_main border border-warning">
-                    <HomeScreen />
-                </Container>
-            </div>
-        </>
-        // <LoginScreen />
-    )
+         <Route path='/auth'>
+            <LoginScreen />
+         </Route>
+
+         <Route path='/search'>
+            <Layout>
+               <h1>Search Results</h1>
+            </Layout>
+         </Route>
+
+         <Route>
+            <Redirect to='/' />
+         </Route>
+      </Switch>
+      </Router>
+   )
 }
 
 export default App
