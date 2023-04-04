553b423bd9e7c43aa286f67ff3f93d1d3f5bce1b
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-04 19:53:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 
 import Header from './components/header/Header'
-import Sidebar from './components/sidebar/Sidebar'
+import Sidebar from './components/sidebar/SideBar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import LoginScreen from './screens/loginScreen/LoginScreen'
 
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 
 import './_app.scss'
 import { useSelector } from 'react-redux'
@@ -33,30 +33,38 @@ const Layout = ({ children }) => {
 }
 
 const App = () => {
+
+
+
+   const { accessToken, loading } = useSelector(state => state.auth)
+
+   const navigate = useNavigate()
+
+
+   useEffect(() => {
+
+      if (!loading && !accessToken) {
+         navigate('/auth')
+      }
+
+   }, [accessToken, loading, navigate])
+
+
+
    return (
-    <Router>
-      <Switch>
-         <Route path='/' exact>
-            <Layout>
-               <HomeScreen />
-            </Layout>
-         </Route>
+      <Routes>
 
-         <Route path='/auth'>
-            <LoginScreen />
-         </Route>
+         <Route path='/' exact element={<Layout><HomeScreen /></Layout>} />
 
-         <Route path='/search'>
-            <Layout>
-               <h1>Search Results</h1>
-            </Layout>
-         </Route>
 
-         <Route>
-            <Redirect to='/' />
-         </Route>
-      </Switch>
-      </Router>
+         <Route path='/auth' exact element={<LoginScreen />} />
+
+
+         <Route path='/search' exact element={<Layout><h1>Search Results</h1></Layout>} />
+
+
+         <Route exact element={<Navigate to='/' />} />
+      </Routes>
    )
 }
 
