9dfa48f92a469a44620dfb37d2e235a2315eefcd
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-04 19:53:47 +0530

    initial commit

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,12 +1,60 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/compat/app'
 import auth from '../../firebase'
+import { GoogleAuthProvider } from 'firebase/auth'
+import { LOAD_PROFILE, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, LOG_OUT } from '../actionType'
 
-const login = () => async dispatch => {
+export const login = () => async dispatch => {
     try {
 
+        dispatch({
+            type: LOGIN_REQUEST
+        })
+
         const provider = new firebase.auth.GoogleAuthProvider()
+        const res = await auth.signInWithPopup(provider)
+        console.log(res)
+
+        const accessToken = res.credential.accessToken
+        const profile = {
+            name: res.additionalUserInfo.profile.name,
+            photoURL: res.additionalUserInfo.profile.picture
+        }
+
+        sessionStorage.setItem('project-accessToken', accessToken)
+        sessionStorage.setItem('project-user', JSON.stringify(profile))
+
+
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: accessToken
+        })
+
+        dispatch({
+            type: LOAD_PROFILE,
+            payload: profile
+        })
 
     } catch (error) {
+        console.log(error.message)
 
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.message
+        })
     }
+}
+
+
+export const logout = () => async dispatch => {
+
+    await auth.signOut()
+
+    dispatch({
+        type: LOG_OUT
+    })
+
+    sessionStorage.removeItem('project-accessToken')
+    sessionStorage.removeItem('project-user')
+
 }
