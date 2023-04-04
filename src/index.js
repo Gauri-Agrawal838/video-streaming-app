ffacaa27443987b0bb21115708f354d12d1574e6
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-04 19:54:03 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import App from './App';
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import store from "./redux/store";
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+
 
 ReactDOM.render(
     <Provider store={store}>
-        < App />
+        <Router>
+            < App />
+        </Router>
     </Provider>
     , document.getElementById('root'));
