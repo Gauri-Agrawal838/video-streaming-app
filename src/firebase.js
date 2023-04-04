83c3448f7a5c456fe3faac0d601aca90e4a37ae1
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-04 19:53:28 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,18 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
+import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAismaZYPY62RI96aguRYQ0MWwyX9KPXVc",
-    authDomain: "video-streaming-app2.firebaseapp.com",
-    projectId: "video-streaming-app2",
-    storageBucket: "video-streaming-app2.appspot.com",
-    messagingSenderId: "299713106635",
-    appId: "1:299713106635:web:d5d3fb6d302c4fd8c4c14e"
-  };
+  apiKey: "AIzaSyAismaZYPY62RI96aguRYQ0MWwyX9KPXVc",
+  authDomain: "video-streaming-app2.firebaseapp.com",
+  projectId: "video-streaming-app2",
+  storageBucket: "video-streaming-app2.appspot.com",
+  messagingSenderId: "299713106635",
+  appId: "1:299713106635:web:d5d3fb6d302c4fd8c4c14e"
+};
 
 firebase.initializeApp(firebaseConfig)
+
+export default firebase.auth()
