29c63a70122a6e5875548924b204854b0da32aa9
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-04 19:53:53 +0530

    initial commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { authReducer } from './reducres/auth.reducer'
 
-const initialState = {
-    name: 'XYZ',
-    age: 20
-}
+const rootReducer = combineReducers({
+    auth: authReducer
+})
 
-const reducer = initialState => initialState
-
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
