64e881a52eae472331f5a1de4b9a51be079ef0a6
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-11 22:04:20 +0530

    initial commit

--- src/redux/reducres/videos.reducer.js
+++ src/redux/reducres/videos.reducer.js
@@ -2,38 +2,40 @@ import { HOME_VIDEOS_FAIL, HOME_VIDEOS_REQUEST, HOME_VIDEOS_SUCCESS } from "../a
 
 export const homeVideosReducer = (
     state = {
-        videos : [],
-        loading : false,
-        nextPageToken : null,
-        activeCategory : 'All'
+        videos: [],
+        loading: false,
+        nextPageToken: null,
+        activeCategory: 'All'
     },
     action
 ) => {
-    const {type, payload} = action
+    const { type, payload } = action
 
     switch (type) {
-        case HOME_VIDEOS_SUCCESS :
-        return {
-            ...state,
-            videos : payload.videos,
-            loading : false,
-            nextPageToken : payload.nextPageToken,
-            activeCategory : payload.category,
-        }
+        case HOME_VIDEOS_SUCCESS:
+            return {
+                ...state,
+                videos:
+                    state.activeCategory === payload.category ? [...state.videos, ...payload.videos] : payload.videos
+                ,
+                loading: false,
+                nextPageToken: payload.nextPageToken,
+                activeCategory: payload.category,
+            }
 
-        case HOME_VIDEOS_FAIL :
-        return {
-            ...state,
-            loading : false,
-            error:payload,
-        }
+        case HOME_VIDEOS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: payload,
+            }
 
-        case HOME_VIDEOS_REQUEST : 
-        return {
-            ...state,
-            loading : true,
-        }
+        case HOME_VIDEOS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
 
-        default : return state
+        default: return state
     }
 }
