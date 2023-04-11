bbf47a5dfd80a678926181ecafef1a867509c06e
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-11 22:04:16 +0530

    initial commit

--- src/redux/actions/videos.action.js
+++ src/redux/actions/videos.action.js
@@ -1,69 +1,69 @@
 import { HOME_VIDEOS_FAIL, HOME_VIDEOS_REQUEST, HOME_VIDEOS_SUCCESS } from "../actionType"
 import request from "../../api"
 
-export const getPopularVideos = () => async dispatch => {
-    try{
+export const getPopularVideos = () => async (dispatch, getState) => {
+    try {
         dispatch({
-            type : HOME_VIDEOS_REQUEST,
+            type: HOME_VIDEOS_REQUEST,
         })
-        const {data} = await request('/videos',{
-            params : {
-                part : 'snippet, contentDetails, statistics',
-                chart : 'mostPopular',
-                regionCode : 'IN',
-                maxResults : 20,
-                pageToken : '',
+        const { data } = await request('/videos', {
+            params: {
+                part: 'snippet, contentDetails, statistics',
+                chart: 'mostPopular',
+                regionCode: 'IN',
+                maxResults: 20,
+                pageToken: getState().homeVideos.nextPageToken,
             },
         })
 
 
         dispatch({
-            type : HOME_VIDEOS_SUCCESS,
-            payload : {
-                videos : data.items,
-                nextPageToken : data.nextPageToken,
+            type: HOME_VIDEOS_SUCCESS,
+            payload: {
+                videos: data.items,
+                nextPageToken: data.nextPageToken,
             },
         })
 
     } catch (error) {
         console.log(error.message)
         dispatch({
-            type : HOME_VIDEOS_FAIL,
-            payload :  error.message,
+            type: HOME_VIDEOS_FAIL,
+            payload: error.message,
         })
     }
 }
 
-export const getVideosByCategory = (keyword) => async (dispatch,getState) => {
-    try{
+export const getVideosByCategory = (keyword) => async (dispatch, getState) => {
+    try {
         dispatch({
-            type : HOME_VIDEOS_REQUEST,
+            type: HOME_VIDEOS_REQUEST,
         })
-        const {data} = await request('/search',{
-            params : {
-                part : 'snippet',
-                maxResults : 20,
-                pageToken : getState().homeVideos.nextPageToken,
-                q : keyword,
-                type : 'video'
+        const { data } = await request('/search', {
+            params: {
+                part: 'snippet',
+                maxResults: 20,
+                pageToken: getState().homeVideos.nextPageToken,
+                q: keyword,
+                type: 'video'
             },
         })
 
 
         dispatch({
-            type : HOME_VIDEOS_SUCCESS,
-            payload : {
-                videos : data.items,
-                nextPageToken : data.nextPageToken,
-                category  : 'All'
+            type: HOME_VIDEOS_SUCCESS,
+            payload: {
+                videos: data.items,
+                nextPageToken: data.nextPageToken,
+                category: 'All'
             },
         })
 
     } catch (error) {
         console.log(error.message)
         dispatch({
-            type : HOME_VIDEOS_FAIL,
-            payload :  error.message,
+            type: HOME_VIDEOS_FAIL,
+            payload: error.message,
         })
     }
 }
