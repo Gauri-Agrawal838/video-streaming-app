34e2e8f2fdd2062ec4c7f4a85da167c1d35eb159
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-11 10:43:30 +0530

    initial commit

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./_video.scss"
 
 import { AiFillEye } from 'react-icons/ai'
@@ -6,44 +6,44 @@ import request from '../../api'
 import moment from 'moment/moment'
 import numeral from 'numeral'
 
-const Video = ({video}) => {
+const Video = ({ video }) => {
 
-  const {id, snippet:{channelId,channelTitle,title,publishedAt,thumbnails:{medium}}} = video
+  const { id, snippet: { channelId, channelTitle, title, publishedAt, thumbnails: { medium } } } = video
 
-  const [views,setViews] = useState(null)
-  const [duration,setDuration] = useState(null)
+  const [views, setViews] = useState(null)
+  const [duration, setDuration] = useState(null)
   const [channelIcon, setChannelIcon] = useState(null)
 
   const seconds = moment.duration(duration).asSeconds()
-  const _duration = moment.utc(seconds*1000).format("mm:ss")
+  const _duration = moment.utc(seconds * 1000).format("mm:ss")
 
   const _videoId = id?.videoId || id;
   useEffect(() => {
-    const get_video_details = async () =>{
-      const {data:{items}} = await request('/videos',{
-        params : {
-          part : 'contentDetails,statistics',
-          id : {_videoId},
+    const get_video_details = async () => {
+      const { data: { items } } = await request('/videos', {
+        params: {
+          part: 'contentDetails,statistics',
+          id: { _videoId },
         }
       }[_videoId])
       setDuration(items[0].contentDetails.duration)
       setViews(items[0].statistics.viewCount)
     }
     get_video_details()
-  },[id])
-  
+  }, [id])
+
   useEffect(() => {
-    const get_channel_icon = async () =>{
-      const {data:{items}} = await request('/channels',{
-        params : {
-          part : 'snippet',
-          id : channelId,
+    const get_channel_icon = async () => {
+      const { data: { items } } = await request('/channels', {
+        params: {
+          part: 'snippet',
+          id: channelId,
         }
       })
       setChannelIcon(items[0].snippet.thumbnails.default)
     }
     get_channel_icon()
-  },[channelId])
+  }, [channelId])
 
 
   return (
@@ -70,3 +70,4 @@ const Video = ({video}) => {
 }
 
 export default Video
+
