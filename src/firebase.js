9bb87b7ccef1259588591bf5787559fcf13ac447
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-11 10:43:37 +0530

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // import firebase from 'firebase/compat/app';
 import firebase from 'firebase/app';
-// import 'firebase/compat/auth'
-// import 'firebase/compat/firestore'
+import 'firebase/auth'
+import 'firebase/firestore'
 
 import 'firebase/auth'
 
