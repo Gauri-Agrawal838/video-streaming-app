471003026d8c06d4b04bfea625ea20656096722c
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-12 22:07:58 +0530

    initial commit

---
+++ src/components/videoHorizontal/VideoHorizontal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_videoHorizontal.scss'
+
+const VideoHorizontal = () => {
+    return (
+        <div>
+            Video Horizontal
+        </div>
+    )
+}
+
+export default VideoHorizontal
