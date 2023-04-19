9acd3f5d97a08adfa598a7d74975c8cd552a28ad
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-19 16:28:11 +0530

    Comment Section updated

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,35 +1,58 @@
-import React from 'react'
-import './_comments.scss'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+   addComment,
+   getCommentsOfVideoById,
+} from '../../redux/actions/comments.action'
 import Comment from '../comment/Comment'
+import './_comments.scss'
+const Comments = ({ videoId, totalComments }) => {
+   const dispatch = useDispatch()
 
-const Comments = () => {
+   useEffect(() => {
+      dispatch(getCommentsOfVideoById(videoId))
+   }, [videoId, dispatch])
 
-    const handleSubmit = () => {
+   const comments = useSelector(state => state.commentList.comments)
+   const { photoURL } = useSelector(state => state.auth?.user)
 
-    }
+   const [text, setText] = useState('')
 
-    return (
-        <div className='comments'>
-            <p>1234 Comments</p>
-            <div className='comments_form d-flex w-100 my-2'>
-                <img
-                    src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
-                    alt=''
-                    className='rounded-circle mr-3'
-                />
-                <form onSubmit={handleSubmit} className='d-flex flex-grow-1'>
-                    <input type='text' className='flex-grow-1' placeholder='Write a comment...' />
-                    <button className='border-0 p-2'>Comment</button>
-                </form>
-            </div>
+   const _comments = comments?.map(
+      comment => comment.snippet.topLevelComment.snippet
+   )
 
-            <div className='comments_list'>
-                {
-                    [...Array(15)].map(() => <Comment />)
-                }
-            </div>
-        </div>
-    )
+   const handleComment = e => {
+      e.preventDefault()
+      if (text.length === 0) return
+
+      dispatch(addComment(videoId, text))
+
+      setText('')
+   }
+   return (
+      <div className='comments'>
+         <p>{totalComments} Comments</p>
+         <div className='my-2 comments__form d-flex w-100'>
+            <img src={photoURL} alt='avatar' className='mr-3 rounded-circle' />
+            <form onSubmit={handleComment} className='d-flex flex-grow-1'>
+               <input
+                  type='text'
+                  className='flex-grow-1'
+                  placeholder='Write a comment...'
+                  value={text}
+                  onChange={e => setText(e.target.value)}
+               />
+               <button className='p-2 border-0'>Comment</button>
+            </form>
+         </div>
+         <div className='comments__list'>
+            {_comments?.map((comment, i) => (
+               <Comment comment={comment} key={i} />
+            ))}
+         </div>
+      </div>
+   )
 }
 
 export default Comments
