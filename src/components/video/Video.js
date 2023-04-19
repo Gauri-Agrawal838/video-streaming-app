1e5b9a8441bba9aacad6e453d82bf794c74da497
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-19 16:28:26 +0530

    New video added

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,90 +1,90 @@
 import React, { useEffect, useState } from 'react'
-import "./_video.scss"
+import './_video.scss'
 
 import { AiFillEye } from 'react-icons/ai'
 import request from '../../api'
-import moment from 'moment/moment'
+
+import moment from 'moment'
 import numeral from 'numeral'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { useNavigate } from 'react-router-dom'
+const Video = ({ video, channelScreen }) => {
+   const {
+      id,
+      snippet: {channelId,channelTitle,title,publishedAt,thumbnails: { medium },
+      },
+      contentDetails,
+   } = video
 
+   const [views, setViews] = useState(null)
+   const [duration, setDuration] = useState(null)
+   const [channelIcon, setChannelIcon] = useState(null)
 
- 
+   const seconds = moment.duration(duration).asSeconds()
+   const _duration = moment.utc(seconds * 1000).format('mm:ss')
 
-const Video = ({ video }) => {
+   const _videoId = id?.videoId || contentDetails?.videoId || id
 
-  const { id, snippet: { channelId, channelTitle, title, publishedAt, thumbnails: { medium } } } = video
+   const navigate = useNavigate()
 
-  const [views, setViews] = useState(null)
-  const [duration, setDuration] = useState(null)
-  const [channelIcon, setChannelIcon] = useState(null)
+   useEffect(() => {
+      const get_video_details = async () => {
+         const {
+            data: { items },
+         } = await request('/videos', {
+            params: {
+               part: 'contentDetails,statistics',
+               id: _videoId,
+            },
+         })
+         setDuration(items[0].contentDetails.duration)
+         setViews(items[0].statistics.viewCount)
+      }
+      get_video_details()
+   }, [_videoId])
 
-  const seconds = moment.duration(duration).asSeconds()
-  const _duration = moment.utc(seconds * 1000).format("mm:ss")
+   useEffect(() => {
+      const get_channel_icon = async () => {
+         const {
+            data: { items },
+         } = await request('/channels', {
+            params: {
+               part: 'snippet',
+               id: channelId,
+            },
+         })
+         setChannelIcon(items[0].snippet.thumbnails.default)
+      }
+      get_channel_icon()
+   }, [channelId])
 
-  const _videoId = id?.videoId || id;
+   const handleVideoClick = () => {
+      navigate(`/watch/${_videoId}`)
+   }
 
-  const navigate = useNavigate()
+   return (
+      <div className='video' onClick={handleVideoClick}>
+         <div className='video_top'>
+            {/* <img src={medium.url} alt='' /> */}
+            <LazyLoadImage src={medium.url} effect='blur' />
+            <span className='video_top_duration'>{_duration}</span>
+         </div>
+         <div className='video_title'>{title}</div>
+         <div className='video_details'>
+            <span>
+               <AiFillEye /> {numeral(views).format('0.a')} Views •{' '}
+            </span>{' '}
+            <span> {moment(publishedAt).fromNow()} </span>
+         </div>
+         {!channelScreen && (
+            <div className='video__channel'>
+               <LazyLoadImage src={channelIcon?.url} effect='blur' />
 
-
-  useEffect(() => {
-    const get_video_details = async () => {
-      const { data: { items } } = await request('/videos', {
-        params: {
-          part: 'contentDetails,statistics',
-          id: { _videoId },
-        }
-      }[_videoId])
-      setDuration(items[0].contentDetails.duration)
-      setViews(items[0].statistics.viewCount)
-    }
-    get_video_details()
-  }, [id])
-
-  useEffect(() => {
-    const get_channel_icon = async () => {
-      const { data: { items } } = await request('/channels', {
-        params: {
-          part: 'snippet',
-          id: channelId,
-        }
-      })
-      setChannelIcon(items[0].snippet.thumbnails.default)
-    }
-    get_channel_icon()
-  }, [channelId])
-
-
-  const handleVideoClick = () => {
-    navigate(`/watch/${_videoId}`)
-  }
-
-
-
-  return (
-    <div className='video' onClick={handleVideoClick}>
-      <div className='video_top'>
-        {/* <img src={medium.url} alt='' /> */}
-        <LazyLoadImage src={medium.url} effect='blur' />
-        <span className='video_top_duration'>{_duration}</span>
+               <p>{channelTitle}</p>
+            </div>
+         )}
       </div>
-      <div className='video_title'>
-        {title}
-      </div>
-      <div className='video_details'>
-        <span>
-          <AiFillEye /> {numeral(views).format("0.a")} Views •
-        </span>
-        <span>{moment(publishedAt).fromNow()}</span>
-      </div>
-      <div className='video_channel'>
-        {/* <img src={channelIcon?.url} alt='' /> */}
-        <LazyLoadImage src={channelIcon?.url} effect='blur' />
-        <p>{channelTitle}</p>
-      </div>
-    </div>
-  )
+   )
 }
 
 export default Video
-
