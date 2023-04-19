59973c3527445caf17e4f49545558e59f0fe84ac
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-19 16:28:07 +0530

    Comment Section updated

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import './_comment.scss'
 import moment from 'moment'
+import './_comment.scss'
+const Comment = ({ comment }) => {
+   const {
+      authorDisplayName,
+      authorProfileImageUrl,
+      publishedAt,
+      textDisplay,
+   } = comment
 
-const Comment = () => {
-    return (
-        <div className='comment p-2 d-flex'>
-            <img
-                src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
-                alt=''
-                className='rounded-circle mr-3'
-            />
-            <div className='comment_body'>
-                <p className='comment_header mb-1'>Lokesh Sharma • {moment('2021-10-14').fromNow()}</p>
-                <p className='mb-0'>Nice Video</p>
-            </div>
-        </div>
-    )
+   return (
+      <div className='p-2 comment d-flex'>
+         <img
+            src={authorProfileImageUrl}
+            alt=''
+            className='mr-3 rounded-circle'
+         />
+         <div className='comment__body'>
+            <p className='mb-1 comment__header'>
+               {authorDisplayName} • {moment(publishedAt).fromNow()}
+            </p>
+            <p className='mb-0'>{textDisplay}</p>
+         </div>
+      </div>
+   )
 }
 
 export default Comment
