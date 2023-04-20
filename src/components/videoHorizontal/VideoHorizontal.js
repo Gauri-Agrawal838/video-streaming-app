d5da2d31907753a5b0293ee283a48c49add3aac2
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-20 12:17:11 +0530

    updated

--- src/components/videoHorizontal/VideoHorizontal.js
+++ src/components/videoHorizontal/VideoHorizontal.js
@@ -1,49 +1,114 @@
-import React from 'react'
-import './_videoHorizontal.scss'
-import { AiFillEye } from 'react-icons/ai'
-import request from '../../api'
-import moment from 'moment/moment'
-import numeral from 'numeral'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import { Row, Col } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import "./_videoHorizontal.scss";
+import { AiFillEye } from "react-icons/ai";
+import request from "../../api";
+import moment from "moment";
+import numeral from "numeral";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Row, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
+const VideoHorizontal = ({ video, searchScreen }) => {
+  const {
+    id,
+    snippet: {
+      channelId,
+      channelTitle,
+      description,
+      title,
+      publishedAt,
+      thumbnails: { medium },
+    },
+  } = video;
 
-const VideoHorizontal = () => {
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [channelIcon, setChannelIcon] = useState(null);
 
+  const isVideo = !(id.kind === "youtube#channel");
+  useEffect(() => {
+    const get_video_details = async () => {
+      const {
+        data: { items },
+      } = await request("/videos", {
+        params: {
+          part: "contentDetails,statistics",
+          id: id.videoId,
+        },
+      });
+      setDuration(items[0].contentDetails.duration);
+      setViews(items[0].statistics.viewCount);
+    };
+    if (isVideo) get_video_details();
+  }, [id, isVideo]);
 
-    const seconds = moment.duration('100').asSeconds()
-    const _duration = moment.utc(seconds * 1000).format("mm:ss")
+  useEffect(() => {
+    const get_channel_icon = async () => {
+      const {
+        data: { items },
+      } = await request("/channels", {
+        params: {
+          part: "snippet",
+          id: channelId,
+        },
+      });
+      setChannelIcon(items[0].snippet.thumbnails.default);
+    };
+    get_channel_icon();
+  }, [channelId]);
 
+  const seconds = moment.duration(duration).asSeconds();
+  const _duration = moment.utc(seconds * 1000).format("mm:ss");
 
-    return (
-        <Row className='videoHorizontal m-1 py-2 align-items-center'>
-            <Col xs={6} md={4} className='videoHorizontal_left'>
-                <LazyLoadImage
-                    src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
-                    effect='blur'
-                    className='videoHorizontal_thumbnail'
-                    wrapperClassName='videoHorizontal_thumbnail-wrapper'
-                />
-                <span className='video_top_duration'>{_duration}</span>
-            </Col>
-            <Col xs={6} md={8} className='videoHorizontal_right p-0'>
-                <p className='videoHorizontal_title mb-1'>
-                    Be a FullStack Developer in 1 month
-                </p>
-                <div className='videoHorizontal_details'>
-                    <AiFillEye /> {numeral(100000).format("0.a")} Views •
-                    {moment('2023-01-02').fromNow()}
-                </div>
-                <div className='videoHorizontal_channel d-flex align-items-center mt-1'>
-                    {/* <LazyLoadImage
-                        src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
-                        effect='blur'
-                    /> */}
-                    <p>Clark Holden</p>
-                </div>
-            </Col>
-        </Row>
-    )
-}
+  const navigate = useNavigate();
+  const handleClick = () => {
+    
+    isVideo? navigate(`/watch/${id.videoId}`): navigate(`/channel/${id.channelId}`)
+  };
 
-export default VideoHorizontal
+  const thumbnail = !isVideo && "videoHorizontal_thumbnail-channel";
+
+  return (
+    <Row
+      className="m-1 py-2 videoHorizontal align-items-center"
+      onClick={handleClick}
+    >
+      <Col xs={6} md={searchScreen ? 4 : 6} className="videoHorizontal_left">
+        <LazyLoadImage
+          src={medium.url}
+          effect="blur"
+          className={`videoHorizontal_thumbnail ${thumbnail} `}
+          wrapperClassName="videoHorizontal_thumbnail-wrapper"
+        />
+
+        {isVideo && (
+          <span className="videoHorizontal_duration">{_duration}</span>
+        )}
+
+      </Col>
+      <Col
+        xs={6}
+        md={searchScreen ? 8 : 6}
+        className="videoHorizontal_right p-0"
+      >
+        <p className="videoHorizontal_title mb-1">{title}</p>
+
+        {isVideo && (
+          <div className="videoHorizontal_details">
+            <AiFillEye /> {numeral(views).format("0.a")} Views •
+            {moment(publishedAt).fromNow()}
+          </div>
+        )}
+
+        {isVideo && <p className="mt-1">{description}</p>}
+        <div className="videoHorizontal_channel d-flex align-items-center mt-1">
+          {/* //TODO show in search screen */}
+          {isVideo && <LazyLoadImage src={channelIcon?.url} effect="blur" />}
+          <p className="mb-0">{channelTitle}</p>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default VideoHorizontal;
