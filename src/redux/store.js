909c96f7bbcf66026ab1459bdd07cf6949bf8696
Gauri-Agrawal838 <gauri.agrawal_cs20@gla.ac.in>
2023-04-20 12:18:04 +0530

    search action added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,19 @@ import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { authReducer } from './reducres/auth.reducer'
-import { homeVideosReducer, selectedVideoReducer } from './reducres/videos.reducer';
+import { homeVideosReducer, searchedVideosReducer, selectedVideoReducer } from './reducres/videos.reducer';
 import {channelDetailsReducer} from './reducres/channel.reducer'
 import { commentListReducer } from './reducres/comments.reducer';
+import { relatedVideoReducer } from './reducres/videos.reducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     homeVideos: homeVideosReducer,
     selectedVideo: selectedVideoReducer,
     channelDetails: channelDetailsReducer,
-    commentList: commentListReducer
+    commentList: commentListReducer,
+    relatedVideos: relatedVideoReducer,
+    searchedVideos : searchedVideosReducer
 })
 
 const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)))
