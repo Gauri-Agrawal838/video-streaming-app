690421111580a5c1d5b2800b4039d29754bbfaf7
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-21 22:26:50 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 console.log(process.env.REACT_APP_YT_API_KEY)
 const request = axios.create({
-    baseURL : 'https://youtube.googleapis.com/youtube/v3/',
-    params : {
-        key : "AIzaSyAismaZYPY62RI96aguRYQ0MWwyX9KPXVc",
+    baseURL: 'https://youtube.googleapis.com/youtube/v3/',
+    params: {
+        key: "AIzaSyAismaZYPY62RI96aguRYQ0MWwyX9KPXVc",
     },
 })
 
