df12eebad3c79cc845ce25a9202d20fc49c06c36
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-21 09:43:55 +0530

    updated

---
+++ src/screens/subscriptionScreen/SubscriptionScreen.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import './_subscriptionScreen.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { getVideosByChannel } from '../../redux/actions/videos.action'
+import { Container } from 'react-bootstrap'
+import VideoHorizontal from '../../components/videoHorizontal/VideoHorizontal'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+
+
+
+const SubscriptionScreen = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getVideosByChannel())
+    }, [dispatch])
+
+
+    const { loading, videos } = useSelector(state => state.subscriptionsChannel)
+
+
+
+    return (
+        <Container fluid className='cont'>
+
+            {
+                !loading ? videos?.map(video => <VideoHorizontal video={video} key={video.id} subScreen />)
+                    : (
+                        <SkeletonTheme color='#343a40' highlightColor='#3c4147'>
+                            <Skeleton width='100%' height='160px' count={20} />
+                        </SkeletonTheme>
+                    )
+            }
+
+        </Container>
+    )
+}
+
+export default SubscriptionScreen
