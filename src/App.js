aba4d53128781df9b8054f3db7d8975e5ba6d330
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-21 22:26:55 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { useSelector } from 'react-redux'
 import WatchScreen from './screens/watchScreen/WatchScreen'
 import SearchScreen from './screens/searchScreen/SearchScreen'
 import SubscriptionScreen from './screens/subscriptionScreen/SubscriptionScreen'
+import ChannelScreen from './screens/channelScreen/ChannelScreen'
 
 
 
@@ -25,12 +26,12 @@ const Layout = ({ children }) => {
    return (
       <>
          <Header handleToggleSidebar={handleToggleSidebar} />
-         <div className='app__container'>
+         <div className='app_container'>
             <Sidebar
                sidebar={sidebar}
                handleToggleSidebar={handleToggleSidebar}
             />
-            <Container fluid className='app__main '>
+            <Container fluid className='app_main '>
                {children}
             </Container>
          </div>
@@ -72,7 +73,7 @@ const App = () => {
 
          <Route path='/feed/subscriptions' exact element={<Layout><SubscriptionScreen /></Layout>} />
 
-         <Route path='/channel/:channelId' exact element={<Layout>Channel Screen</Layout>} />
+         <Route path='/channel/:channelId' exact element={<Layout><ChannelScreen /></Layout>} />
 
 
          <Route exact element={<Navigate to='/' />} />
